e8e2d9d7a5a6b014220a5c8bd433be4599d4380c
Arthur Song <arth3rs0ng@gmail.com>
2019-04-20 03:24:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO series (title, author_id, sub_genre_id) VALUES
-("Into the Darkness", "Michael Ford", "Mystery"),
-("The Castle in the Sky", "Miyazaki", "Adventure");
+("Into the Darkness", 1, 1),
+("The Castle in the Sky", 2, 2);
 
 INSERT INTO sub_genres (name) VALUES
 ("Mystery"),
@@ -10,4 +10,12 @@ INSERT INTO authors (name) VALUES
 ("Michael Ford"),
 ("Miyazaki");
 
-INSERT INTO books ()
+INSERT INTO books (title, year, series_id) VALUES
+("The Mad Man", 1992, 1),
+("The Dark Forest", 1996, 1),
+("The Hidden Land", 2001, 1),
+("Miru", 1973, 2),
+("Flying with Dragons", 1982, 2),
+("The Lost Crystal", 1988, 2);
+
+INSERT INTO characters ()
