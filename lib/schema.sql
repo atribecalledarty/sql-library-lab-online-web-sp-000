61c8a76cd1f9b1aabfd1170d2609ca7b0e232357
Arthur Song <arth3rs0ng@gmail.com>
2019-04-20 06:24:23 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE books (
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
-  series_id INTEGER
+  series_id INTEGER,
+  subgenre_id INTEGER
   );
   
 CREATE TABLE series (
@@ -26,7 +27,7 @@ CREATE TABLE authors (
   name TEXT
   );
   
-CREATE TABLE sub_genres (
+CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
   );
