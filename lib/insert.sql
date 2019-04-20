8efd16b163cd30250cf72d5f9c5073d0147a6f6a
Arthur Song <arth3rs0ng@gmail.com>
2019-04-20 03:21:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,8 @@ INSERT INTO series (title, author_id, sub_genre_id) VALUES
 ("Into the Darkness", "Michael Ford", "Mystery"),
 ("The Castle in the Sky", "Miyazaki", "Adventure");
 
-INSERT INTO sub_genres ()
+INSERT INTO sub_genres (name) VALUES
+("Mystery"),
+("Adventure");
+
+INSERT INTO authors (name)
