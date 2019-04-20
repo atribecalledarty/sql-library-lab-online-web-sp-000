536f6ac02bb26e23be1f22399c81cc7f0c3d5f4f
Arthur Song <arth3rs0ng@gmail.com>
2019-04-20 06:43:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(*)
+  "SELECT series.title
   FROM series
   INNER JOIN books ON books.series_id = series.id
   INNER JOIN character_books ON books.id = character_books.book_id
